52f2291030bebf0654834ae6ea786c63315d7bae
Abdullah Abouhashim <dullah@Mac.attlocal.net>
2024-12-06 03:46:29 -0500

    created login/ logout functionality with authentication

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,23 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-import authRoutes from "./routes/auth.routes.js"
+import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectMongoDB();
 });
