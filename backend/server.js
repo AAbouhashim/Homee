9f92e2d754def44c33f21aee343986cfb58af3a7
Abdullah Abouhashim <dullah@Mac.attlocal.net>
2024-12-18 07:05:37 -0500

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -22,6 +23,7 @@ const corsOptions = {
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -35,6 +37,14 @@ app.use("/api/users", verifyToken, userRoutes);  // Token validation middleware 
 app.use("/api/posts", verifyToken, postRoutes);  // Token validation middleware for posts
 app.use("/api/notifications", verifyToken, notificationRoutes);  // Token validation for notifications
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  }
+  );
+}
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
