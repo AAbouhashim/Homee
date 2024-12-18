84ca297ac8b83075016739b5ca2a4c9474c0bbc7
Abdullah Abouhashim <dullah@Mac.attlocal.net>
2024-12-18 07:17:13 -0500

    prep for dist

--- backend/server.js
+++ backend/server.js
@@ -38,10 +38,10 @@ app.use("/api/posts", verifyToken, postRoutes);  // Token validation middleware 
 app.use("/api/notifications", verifyToken, notificationRoutes);  // Token validation for notifications
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  app.get("*", (_, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   }
   );
 }
